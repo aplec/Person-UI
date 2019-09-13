43c03dbb9e280a8296116b39c3e788f7de98aaf8
Ante Plecas <anteplecas@Antes-MacBook-Pro.local>
2019-09-13 14:26:28 -0500

    First commit

---
+++ Person UI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Person UI
+//
+//  Created by Ante Plecas on 9/13/19.
+//  Copyright © 2019 Ante Plecas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
